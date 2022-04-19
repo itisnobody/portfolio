5743386e29d904494ccd2fdee6bd3c106dd06b52
Sasha <alexander.goryachev@gmail.com>
2022-04-19 22:03:04 +0300

    Update 2022-04-19T19:03:00.500Z

--- src/js/_components.js
+++
@@ -1,6 +0,0 @@
-console.log('components');
-import navMenu from './components/menu';
-import progressBar from './components/progressBar';
-
-navMenu();
-progressBar();
--- src/js/components/progressBar.js
+++
@@ -1,7 +0,0 @@
-const progressBar = function() {
-  const data = document.querySelectorAll('.progress__data'),
-        bar = document.querySelectorAll('.progress__bar span');
-  data.forEach((item, i) => bar[i].style.width = item.innerHTML);
-};
-
-export default progressBar;
